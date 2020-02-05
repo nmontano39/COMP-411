7c5e9a193f4380f42e018eb0d8b358c433a610c4
nicho <nmontano39@gmail.com>
2020-02-04 18:16:48 -0600

    working on interpreter

--- programs/2/src/Interpreter.java
+++ programs/2/src/Interpreter.java
@@ -1,5 +1,30 @@
-public class Interpreter {
+import java.io.IOException;
+import java.io.Reader;
 
+/** file Interpreter.java **/
+class EvalException extends RuntimeException {
+    EvalException(String msg) { super(msg); }
+}
+
+class Interpreter {
+    Interpreter(String fileName) throws IOException {
+
+    }
+
+    Interpreter(Reader reader) {
+
+    }
+
+    public JamVal callByValue() {
+
+    };
+
+    public JamVal callByName()  {
+
+    };
+
+    public JamVal callByNeed()  {
+
+    };
     
-
 }
